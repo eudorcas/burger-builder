a005e7834bc9d68b323d41738d64bd23bd2ffc8f
eudorcas <klaudia.butkiewicz09@gmail.com>
2019-01-23 11:59:03 +0100

    Layout component

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const layout = (props) => (
+    <>
+        <div>
+            Toolbar, SideDrawer, Backdrop
+        </div>
+        <main>
+            {props.children}
+        </main>
+    </>
+);
+
+export default layout;
