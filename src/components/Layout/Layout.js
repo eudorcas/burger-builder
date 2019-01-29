1f53ac5b19cdcd7c7102d1fa44d20b7b76486851
eudorcas <klaudia.butkiewicz09@gmail.com>
2019-01-29 14:31:27 +0100

    Toolbar Component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
     <>
         <div>
-            Toolbar, SideDrawer, Backdrop
+            <Toolbar />
+            SideDrawer, Backdrop
         </div>
         <main className={classes.Content}>
             {props.children}
