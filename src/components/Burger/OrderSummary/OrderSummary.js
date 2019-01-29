01716dad885d9e0615e03f7ced4e18c9d68639f2
eudorcas <klaudia.butkiewicz09@gmail.com>
2019-01-29 13:00:18 +0100

    OrderSummary Component

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const orderSummary = (props) => {
+        const ingredientSummary = Object.keys(props.ingredients)
+            .map(igKey => {
+                return <li key={igKey}>
+                <span style={{textTransform: 'capitalize'}}>
+                {igKey}</span>
+                    : {props.ingredients[igKey]}
+                </li>
+            });
+
+
+        return (
+            <>
+                <h3>Your Order</h3>
+                <p>A delicious burger with following ingredients: </p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p>Continue to Checkout?</p>
+            </>
+        );
+    }
+;
+
+
+export default orderSummary;
