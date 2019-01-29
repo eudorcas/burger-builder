3f367c1c20f3b9af3fd73e24d240b69e5c54d575
eudorcas <klaudia.butkiewicz09@gmail.com>
2019-01-29 10:36:23 +0100

    Improving adding ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,12 +5,18 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient type={igKey} key={igKey + i}/>
             });
-        });
+        })
+        .reduce((prev, curr) => {
+            return prev.concat(curr)
+        }, []);
+    if (transformedIngredients.length===0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type={"bread-top"}/>
