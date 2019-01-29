327798f872d037d9288c6a22d14aadf62a614f5b
eudorcas <klaudia.butkiewicz09@gmail.com>
2019-01-29 18:33:31 +0100

    DrawerToggle Component

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,6 +4,7 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 
+
 const sideDrawer = (props) => {
     let attachedClasses = [classes.SideDrawer, classes.Close];
     if(props.open) {
