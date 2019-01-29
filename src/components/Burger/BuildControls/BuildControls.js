19bfedb92d50764a2918fca99c33c195cc863369
eudorcas <klaudia.butkiewicz09@gmail.com>
2019-01-29 11:31:36 +0100

    Adding dynamically ingredients of the burger

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
 
+const controls = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Meat', type: 'meat'}
+];
 
 const buildControls = (props) => {
-    return <div></div>
+    return <div className={classes.BuildControls}>
+        {controls.map(el => (
+                <BuildControl
+                    key={el.label}
+                    label={el.label}
+                    added={() => props.ingredientAdded(el.type)}/>
+            )
+        )}
+    </div>
 };
 
 export default buildControls;
