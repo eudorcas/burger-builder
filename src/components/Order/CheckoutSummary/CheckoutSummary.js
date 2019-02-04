e9eeef064abc6b99481f7f9da1607d0cc253ce61
eudorcas <klaudia.butkiewicz09@gmail.com>
2019-02-04 18:44:54 +0100

    Outputting the orders

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -9,7 +9,7 @@ const checkoutSummary = (props) => {
     return (
         <div className={classes.CheckoutSummary}>
             <h1>We hope it tastes well!</h1>
-            <div style={{width: "100%", height: "300px", margin: "auto"}}>
+            <div style={{width: "100%", margin: "auto"}}>
                 <Burger ingredients={props.ingredients}/>
             </div>
             <Button clicked={props.checkoutCancelled} btnType="Danger">CANCEL</Button>
