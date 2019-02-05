5bb96103e98dc9c3215afd55baa822fb31450b8a
eudorcas <klaudia.butkiewicz09@gmail.com>
2019-02-05 18:55:49 +0100

    Ingredients Reducer

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,15 +27,15 @@ class BurgerBuilder extends Component {
     };
 
     componentWillMount() {
-        axios.get('https://burger-builder-6f47e.firebaseio.com/ingredients.json')
-            .then(response => {
-                this.setState({
-                    ingredients: response.data
-                });
-            })
-            .catch(error => {
-                this.setState({error: true});
-            });
+        // axios.get('https://burger-builder-6f47e.firebaseio.com/ingredients.json')
+        //     .then(response => {
+        //         this.setState({
+        //             ingredients: response.data
+        //         });
+        //     })
+        //     .catch(error => {
+        //         this.setState({error: true});
+        //     });
     };
 
 
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import reducer from './store/reducers';
+import reducer from './store/reducer';
 
 const store = createStore(reducer);
 
