6fb7713180591d9a65c39dabf0ac245890a7809f
eudorcas <klaudia.butkiewicz09@gmail.com>
2019-02-07 09:59:12 +0100

    Adding a new user, getting a token from firebase

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -23,5 +25,19 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyAVitCmW5vA1oG-YQISAqbh5iC8on-aTm0', authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err));
+            })
     }
 };
