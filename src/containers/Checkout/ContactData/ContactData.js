dff519cc603d9f3de23e4b2a0a69a6404cb5a564
eudorcas <klaudia.butkiewicz09@gmail.com>
2019-02-08 14:16:42 +0100

    Displaying orders for specific user only

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -107,7 +107,9 @@ class ContactData extends Component {
         const order = {
             ingredients: this.props.ings,
             price: this.props.price,
-            orderData: formData
+            orderData: formData,
+            userId: this.props.userId
+
 
         };
         this.props.onOrderBurger(order, this.props.token);
@@ -198,13 +200,14 @@ const mapStateToProps = state => {
       ings: state.burgerBuilder.ingredients,
       price: state.burgerBuilder.totalPrice,
       loading: state.order.loading,
-      token: state.auth.token
+      token: state.auth.token,
+      userId: state.auth.userId
   }
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-      onOrderBurger: (orderData) => dispatch(actionCreators.purchaseBurger(orderData))
+      onOrderBurger: (orderData, token) => dispatch(actionCreators.purchaseBurger(orderData, token))
   }
 };
 
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -25,6 +25,7 @@ export const purchaseBurgerStart = () => {
 export const purchaseBurger = (orderData, token) => {
     return dispatch => {
         dispatch(purchaseBurgerStart());
+        console.log(token);
         axios.post('/orders.json?auth=' + token, orderData)
             .then(response => {
                 dispatch(purchaseBurgerSuccess(response.data.name, orderData))
@@ -61,10 +62,11 @@ export const fetchOrdersStart = () => {
     }
 };
 
-export const fetchOrders = (token) => {
+export const fetchOrders = (token, userId) => {
     return dispatch => {
         dispatch(fetchOrdersStart());
-        axios.get('./orders.json?auth=' + token)
+        const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
+        axios.get('./orders.json' + queryParams)
             .then(res => {
                 const fetchedOrders = [];
                 for (let key in res.data) {
